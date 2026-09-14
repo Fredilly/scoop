1da46617cf155fcdd1296ace044d91e99b4cf41d
Fred E <fredilly@article6.org>
2026-09-14 18:50:54 +0800

    Use Scoop R2 logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
+const logo = "https://assets.scoop.article6.org/brand/scoop-logo.png";
+
 export default function Home() {
   return (
     <main className="min-h-screen bg-white text-black">
+      <img src={logo} alt="Scoop" className="h-10 w-auto" />
       <h1 className="text-6xl font-black">SEE IT. SCOOP IT.</h1>
     </main>
   );
